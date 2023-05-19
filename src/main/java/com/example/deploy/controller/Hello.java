1a8c2294c8f52084142ff6c416e77db6a70400b6
Sanowar72 <atifreza72ar@gmail.com>
2023-05-19 22:37:46 +0530

    checking....

--- src/main/java/com/example/deploy/controller/Hello.java
+++ src/main/java/com/example/deploy/controller/Hello.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
-
     @GetMapping("/home")
     public String home(){
         return "im in home checking for deploye.....";
+    }
+
+    @GetMapping(value = "home2")
+    public String home2(){
+        return "this is home 2.......";
     }
 }
